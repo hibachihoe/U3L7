93de7f94591ba04685c0226c4bfc5dd445ec398c
hibachihoe <43859732+hibachihoe@users.noreply.github.com>
2018-12-10 11:28:08 -0500

    Add files via upload

---
+++ lesson 7/problem3.py
@@ -0,0 +1,11 @@
+print('-'*80)
+print('100th Birthday Program:')
+print("Description: This program asks you for your current age and tells you the year that you will turn 100.")
+print()
+age= input('How old are you? ')
+age= int(age)
+yearsleft= 100 - age
+answer= 2018 + yearsleft
+print(answer)
+print('-'*80)
+print()
